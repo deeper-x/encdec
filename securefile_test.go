57cf59874433830bec97819b5304c50894fa2d27
ju570r1u5 <albertodeprezzo@gmail.com>
2023-06-30 18:04:06 +0200

    enhanced tests

--- securefile_test.go
+++ securefile_test.go
@@ -10,7 +10,7 @@ import (
 
 func TestEncryption(t *testing.T) {
 	key := "test"
-	_, err := Encrypt("./assets/sample.pdf", []byte(key))
+	_, err := Encrypt("./assets/DELEGA_DA_FIRMARE.pdf", []byte(key))
 	if err != nil {
 		t.Error(err)
 	}
@@ -18,32 +18,32 @@ func TestEncryption(t *testing.T) {
 
 func TestDecryption(t *testing.T) {
 	key := "test"
-	_, err := Decrypt("./assets/sample.pdf", []byte(key))
+	_, err := Decrypt("./assets/DELEGA_DA_FIRMARE.pdf", []byte(key))
 	if err != nil {
 		t.Error(err)
 	}
 }
 
 func TestMultipleDecEnc(t *testing.T) {
-	for i := 0; i <= 100; i++ {
+	for i := 0; i <= 1000; i++ {
 		log.Println("Encryption: ", i)
-		_, err := Encrypt("./assets/sample.pdf", []byte(""))
+		_, err := Encrypt("./assets/DELEGA_DA_FIRMARE.pdf", []byte(""))
 		if err != nil {
 			t.Error(err)
 		}
 
-		ftype, err := checkFileType("./assets/sample.pdf")
+		ftype, err := checkFileType("./assets/DELEGA_DA_FIRMARE.pdf")
 		if err != nil {
 			t.Error(err)
 		}
 		log.Println("Encrypted type is", ftype)
 
 		log.Println("Decryption: ", i)
-		_, err = Decrypt("./assets/sample.pdf", []byte(""))
+		_, err = Decrypt("./assets/DELEGA_DA_FIRMARE.pdf", []byte(""))
 		if err != nil {
 			t.Error(err)
 		}
-		ftype, err = checkFileType("./assets/sample.pdf")
+		ftype, err = checkFileType("./assets/DELEGA_DA_FIRMARE.pdf")
 		if err != nil {
 			t.Error(err)
 		}
@@ -53,7 +53,7 @@ func TestMultipleDecEnc(t *testing.T) {
 }
 
 func checkFileType(inFile string) (string, error) {
-	buf, err := ioutil.ReadFile("./assets/sample.pdf")
+	buf, err := ioutil.ReadFile("./assets/DELEGA_DA_FIRMARE.pdf")
 	if err != nil {
 		return "", err
 	}
