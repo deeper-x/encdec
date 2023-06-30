fd4c6b865eba54d846287132c1bf32817199e579
ju570r1u5 <albertodeprezzo@gmail.com>
2023-06-30 17:52:37 +0200

    enhanced tests

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/deeper-x/securefile
 go 1.20
 
 require golang.org/x/crypto v0.10.0
+
+require github.com/h2non/filetype v1.1.3 // indirect
--- securefile_test.go
+++ securefile_test.go
@@ -1,8 +1,11 @@
 package securefile
 
 import (
+	"io/ioutil"
 	"log"
 	"testing"
+
+	"github.com/h2non/filetype"
 )
 
 func TestEncryption(t *testing.T) {
@@ -28,10 +31,37 @@ func TestMultipleDecEnc(t *testing.T) {
 		if err != nil {
 			t.Error(err)
 		}
+
+		ftype, err := checkFileType("./assets/sample.pdf")
+		if err != nil {
+			t.Error(err)
+		}
+		log.Println("Encrypted type is", ftype)
+
 		log.Println("Decryption: ", i)
 		_, err = Decrypt("./assets/sample.pdf", []byte(""))
 		if err != nil {
 			t.Error(err)
 		}
+		ftype, err = checkFileType("./assets/sample.pdf")
+		if err != nil {
+			t.Error(err)
+		}
+		log.Println("Decrypted type is", ftype)
+
 	}
+}
+
+func checkFileType(inFile string) (string, error) {
+	buf, err := ioutil.ReadFile("./assets/sample.pdf")
+	if err != nil {
+		return "", err
+	}
+
+	ftype, err := filetype.Match(buf)
+	if err != nil {
+		return "", err
+	}
+
+	return ftype.MIME.Value, nil
 }
